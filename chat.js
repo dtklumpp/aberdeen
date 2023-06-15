de4c82678cb89a48c37827f3f071709d2aa109db
dtklumpp <dtklumpp@gmail.com>
2023-06-15 17:22:38 -0400

    fix mediaError

--- chat.js
+++ chat.js
@@ -1,10 +1,32 @@
-const socket = new WebSocket("ws://localhost:8000");
+//WEBSOCKETS
+
 // const socket = new WebSocket("ws://localhost:8080");
+// // const socket = new WebSocket("ws://localhost:8080");
 
-socket.addEventListener("open", (event) => {
-    socket.send("Hello Server!");
-});
+// socket.addEventListener("open", (event) => {
+//     socket.send("Hello Server!");
+// });
 
-socket.addEventListener("message", (event) => {
-    console.log("Message from server ", event.data);
-});
+// socket.addEventListener("message", (event) => {
+//     console.log("Message from server ", event.data);
+// });
+
+
+
+//WEBRTC
+
+const pc = new RTCPeerConnection();
+const dc = pc.createDataChannel("my channel");
+
+dc.onmessage = (event) => {
+  console.log(`received: ${event.data}`);
+};
+
+dc.onopen = () => {
+  console.log("datachannel open");
+};
+
+dc.onclose = () => {
+  console.log("datachannel close");
+};
+
