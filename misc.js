d9bc86cab6ee687aadc7150591618446468407f7
dtklumpp <dtklumpp@gmail.com>
2023-06-27 14:44:55 -0400

    add clipboard pasting

--- misc.js
+++ misc.js
@@ -4,3 +4,17 @@ const focusit = document.getElementById('focusit');
 const tofocus = document.getElementById('tofocus');
 focusit.onclick = () => tofocus.focus();
 
+const pasteTarget = document.getElementById('paste-target');
+const divTarget = document.getElementById('div-target');
+async function pasteit(){
+    console.log('pasteit fxn');
+    navigator.clipboard.writeText("PASTED IT BAM")
+
+    //oh wow this needs permissions
+    let clip = await navigator.clipboard.readText();
+
+    pasteTarget.value = clip;
+
+    divTarget.innerText = clip;
+}
+
